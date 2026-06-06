b158b7e16029059d90a2e93ee6a2fbe4bb54d507
brnhxxb <kenumi.re@gmail.com>
2026-06-06 21:34:39 +0000

    new1

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
-from flask import Flask, jsonify
-import random
+from flask import Flask
 
 app = Flask(__name__)
 
-counter = 0
+@app.route("/")
+def home():
+    return "OK"
 
-@app.route('/random')
-def rnd():
-    global counter
-    counter += 1
-
-    return jsonify({
-        "number": random.randint(0, 65535)
-    })
-
-@app.route("/metrics")
-def metrics():
-    return jsonify({
-        "count": counter
-    })
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
